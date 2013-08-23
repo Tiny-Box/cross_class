dd744a1b0e3b0016b138e0e15843ad9e05d629fd
airfan13 <2820655657@qq.com>
2013-08-23 20:07:57 +0800

    improved game

--- game.py
+++ game.py
@@ -5,11 +5,19 @@ pygame.init()
 screen = pygame.display.set_mode((510, 510), 0, 32)
 pygame.display.set_caption("Hello, World!")
 background = pygame.image.load('bg.jpg').convert()
+background2 = pygame.image.load('bg2.jpg').convert()
+flag = True
 
 while True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			exit()
-	screen.blit(background, (0,0))
-	pygame.display.update()
+		if event.type == pygame.MOUSEBUTTONDOWN:
+			flag = not flag
+	if flag:
+		screen.blit(background, (0,0))
+		pygame.display.update()
+	else:
+		screen.blit(background2, (0,0))
+		pygame.display.update()
